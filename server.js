d75177851491f55d0b0c4245576d1ad390367371
sergiocotiazure20262-lang <sergiocotiazure20262@gmail.com>
2026-04-18 00:46:45 -0300

    adicionando workflow de deploy

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({
-    mensagem: 'API publicada com pipeline no EC2 com sucesso!'
-  });
+  res.json({ mensagem: 'API publicada com sucesso no EC2!' });
 });
 
 app.get('/status', (req, res) => {
-  res.json({
-    aplicacao: 'api-aula02',
-    status: 'online'
-  });
+  res.json({ status: 'online' });
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
